1d17a3ba4324cc2d61dc8f109acca9783e010e6d
Mahmoud Sayed <mahmoudsayed1612@gmail.com>
2024-07-01 18:52:05 +0300

    Estaplish DB Connection

--- DB/db.connection.js
+++ DB/db.connection.js
@@ -1,1 +1,16 @@
- 
+import mysql2 from 'mysql2';
+export const db_connection = mysql2.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'ecommerce_db'
+});
+
+db_connection.connect(err=>{
+    if(err){
+        console.log("Connection error: " + err);
+    }
+    else{
+        console.log("Connection established successfully");
+    }
+})
