578c16d9a633ed7075e42b34ea70cb2f4637dfb5
Mahmoud Sayed <mahmoudsayed1612@gmail.com>
2024-07-02 16:47:17 +0300

    Finish Customers endpoints

--- Src/Modules/Customers/customers.controller.js
+++ Src/Modules/Customers/customers.controller.js
@@ -103,4 +103,144 @@ export const removeCustomer = (req, res) => {
       }
     }
   });
+};
+
+// *  Customers haven't made any orders
+export const getCustomersWithoutOrder = (req, res) => {
+  const query = `
+  SELECT c.id, c.first_name, c.last_name
+  FROM customers c
+  LEFT JOIN orders o ON c.id = o.customer_id
+  WHERE o.id IS NULL
+`;
+
+  db_connection.query(query, (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.json(results);
+  });
+};
+
+// * Get Customer with Most Items Purchased
+export const mostItemsPurchased = (req, res) => {
+  const query = `
+    SELECT c.id, c.first_name, c.last_name, SUM(oi.quantity) AS total_items
+FROM customers c
+JOIN orders o ON c.id = o.customer_id
+JOIN order_items oi ON o.id = oi.order_id
+GROUP BY c.id
+ORDER BY total_items DESC
+LIMIT 1;
+  `;
+  db_connection.query(query, (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    if (!results.length) return res.json({ message: "No items purchased" });
+    res.json(results[0]);
+  });
+};
+
+// * Get most spending customers
+export const mostSpending = (req, res) => {
+  const limit = parseInt(req.params.limit, 10) || 10;
+
+  if (limit <= 0) {
+    return res.status(400).json({ error: "Limit must be a positive integer" });
+  }
+  const query = `
+    SELECT c.id, c.first_name, c.last_name, SUM(o.total_amount) AS total_spent
+    FROM customers c
+    JOIN orders o ON c.id = o.customer_id
+    GROUP BY c.id
+    ORDER BY total_spent DESC
+    LIMIT ${limit}
+  `;
+  db_connection.query(query, (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    !results.length
+      ? res.json({ message: "No customers spent any money" })
+      : res.json(results);
+  });
+};
+
+// * Get customers with atleast num of orders
+export const minimumOrders = (req, res) => {
+  const limit = parseInt(req.params.limit, 10) || 10;
+
+  if (limit <= 0) {
+    return res.status(400).json({ error: "Limit must be a positive integer" });
+  }
+  const query = `
+        SELECT c.id, c.first_name, c.last_name, COUNT(o.id) AS total_orders
+        FROM customers c
+        JOIN orders o ON c.id = o.customer_id
+        GROUP BY c.id
+        HAVING total_orders >= ${limit}
+    `;
+
+  db_connection.query(query, (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    !results.length
+      ? res.json({ message: `No customers made atleast ${limit} order` })
+      : res.json(results);
+  });
+};
+
+// * Get percentage of multi-order customers
+export const multiOrderPercentage = (req, res) => {
+  const totalCustomersQuery =
+    "SELECT COUNT(*) AS total_customers FROM customers";
+  const multipleOrdersQuery = `
+      SELECT COUNT(DISTINCT customer_id) AS customers_with_multiple_orders
+      FROM orders
+      GROUP BY customer_id
+      HAVING COUNT(id) > 1
+  `;
+
+  db_connection.query(totalCustomersQuery, (err, totalCustomersResult) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+
+    const totalCustomers = totalCustomersResult[0].total_customers;
+
+    db_connection.query(multipleOrdersQuery, (err, multipleOrdersResult) => {
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+
+      const customersWithMultipleOrders = multipleOrdersResult.length;
+      const percentage = (customersWithMultipleOrders / totalCustomers) * 100;
+
+      res.json({ 
+        customersWithMultipleOrders,
+        totalCustomers,
+        percentage: percentage.toFixed(2) });
+    });
+  });
+};
+
+// * Get customer with earliest order
+export const earliestOrder = (req, res) => {
+  const query = `
+        SELECT c.id, c.first_name, c.last_name, MIN(o.order_date) AS earliest_order_date
+        FROM customers c
+        JOIN orders o ON c.id = o.customer_id
+        GROUP BY c.id
+        ORDER BY earliest_order_date ASC
+        LIMIT 1
+    `;
+
+    db_connection.query(query, (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+      results.length? res.json(results[0]): res.json({message: "No orders done yet"});
+    });
 }
--- Src/Modules/Customers/customers.routes.js
+++ Src/Modules/Customers/customers.routes.js
@@ -4,5 +4,11 @@ const customerRouter = Router()
 customerRouter.post('/signup' , customerController.sugnUp)
 customerRouter.post('/login' , customerController.login)
 customerRouter.delete('/removeCustomer' , customerController.removeCustomer)
+customerRouter.get('/noOrders', customerController.getCustomersWithoutOrder)
+customerRouter.get('/mostItems', customerController.mostItemsPurchased)
+customerRouter.get('/mostspend/:limit?', customerController.mostSpending)
+customerRouter.get('/minOrders/:limit?', customerController.minimumOrders)
+customerRouter.get('/multiOrderPercentage', customerController.multiOrderPercentage)
+customerRouter.get('/earliestOrder', customerController.earliestOrder)
 
 export default customerRouter
